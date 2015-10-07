358054591eebd8df8ed9ccd7f52633d83455c916
Jose Suniaga <josemiguel@vauxoo.com>
2015-10-07 16:30:39 -0430

    [REF] petstore.js: testing class attributes

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -11,12 +11,13 @@ openerp.oepetstore = function(instance, local) {
 
     var MyClass = instance.web.Class.extend({
         say_hello: function() {
-            console.log("hello");
+            console.log("hello", this.name);
         },
     });
 
     var my_object = new MyClass();
-	my_object.say_hello();
+    my_object.name = "Bob";
+    my_object.say_hello();
 
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
 }
