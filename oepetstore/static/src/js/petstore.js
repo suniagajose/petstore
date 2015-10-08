6e97a6eeb4d0937a5a8a891dd60a37342ac08c72
Jose Suniaga <josemiguel@vauxoo.com>
2015-10-08 11:06:34 -0430

    [REF] petstore.js, petstore.py: testing communication with Odoo server

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -3,37 +3,15 @@ openerp.oepetstore = function(instance, local) {
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;
 
-
-    local.ColorInputWidget = instance.Widget.extend({
-        template: "ColorInputWidget",
-        events: {
-            'change input': 'input_changed'
-        },
-        start: function() {
-            this.input_changed();
-            return this._super();
-        },
-        input_changed: function() {
-            var color = [
-                "#",
-                this.$(".oe_color_red").val(),
-                this.$(".oe_color_green").val(),
-                this.$(".oe_color_blue").val(),
-            ].join('');
-            this.set("color", color);
-        },
-    });
-
     local.HomePage = instance.Widget.extend({
-        template: "HomePage",
         start: function() {
-            this.colorInput = new local.ColorInputWidget(this);
-            this.colorInput.on("change:color", this, this.color_changed);
-            return this.colorInput.appendTo(this.$el);
+        	var self = this;
+        	var model = new instance.web.Model("oepetstore.message_of_the_day");
+            model.call("my_method", {context: new instance.web.CompoundContext()}).then(function(result) {
+                self.$el.append("<div>Hello " + result["hello"] + "</div>");
+                // will show "Hello world" to the user
+            });
         },
-        color_changed: function() {
-            this.$(".oe_color_div").css("background-color", this.colorInput.get("color"));
-        }
     });
 
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
