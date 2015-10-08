05c4da7c4eab7cd9d5c5f1849e9c101d0716c214
Jose Suniaga <josemiguel@vauxoo.com>
2015-10-08 10:19:11 -0430

    [REF] petstore.js: testing Widget properties

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -5,33 +5,17 @@ openerp.oepetstore = function(instance, local) {
 
     local.HomePage = instance.Widget.extend({
         start: function() {
-            var widget = new local.ConfirmWidget(this);
-            widget.on("user_chose", this, this.user_chose);
-            widget.appendTo(this.$el);
+            var myWidget = new local.MyWidget(this);
+            this.widget = myWidget;
+            this.widget.on("change:name", this, this.name_changed);
+            this.widget.set("name", "Nicolas");
         },
-        user_chose: function(confirm) {
-            if (confirm) {
-                console.log("The user agreed to continue");
-            } else {
-                console.log("The user refused to continue");
-            }
-        },
+        name_changed: function() {
+            console.log("The new value of the property 'name' is", this.widget.get("name"));
+        }
     });
 
-    local.ConfirmWidget = instance.Widget.extend({
-        events: {
-            'click button.ok_button': function () {
-                this.trigger('user_chose', true);
-            },
-            'click button.cancel_button': function () {
-                this.trigger('user_chose', false);
-            }
-        },
-        start: function() {
-            this.$el.append("<div>Are you sure you want to perform this action?</div>" +
-                            "<button class='ok_button'>Ok</button>" +
-                            "<button class='cancel_button'>Cancel</button>");
-        },
+    local.MyWidget = instance.Widget.extend({
     });
 
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
