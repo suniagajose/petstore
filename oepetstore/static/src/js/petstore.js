af11c8f077f65ee7a423f20b10434597e7275f98
Jose Suniaga <josemiguel@vauxoo.com>
2015-10-08 02:04:09 -0430

    [REF] petstore.js, petstore.xml, petstore.css: Exercise

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -3,48 +3,22 @@ openerp.oepetstore = function(instance, local) {
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;
 
-    local.GreetingsWidget = instance.Widget.extend({
-        className: 'oe_petstore_greetings',
-        init: function(parent, name) {
-            // FYI: When overriding the init() method of a
-            // widget you must pass parent to the super() method
-            this._super(parent);  // <-- Like this!
-            this.name = name;
-        },
-        start: function() {
-            console.log(this.getParent().$el );
-        },
-    });
-
     local.HomePage = instance.Widget.extend({
-        className: 'oe_petstore_homepage',
-        template: "HomePageTemplate",
-        init: function(parent) {
-            this._super(parent);
-            this.name = "Mordecai";
-        },
         start: function() {
+            var products = new local.ProductsWidget(
+                this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
+            products.appendTo(this.$el);
         },
     });
 
-    var MyClass = instance.web.Class.extend({
-        init: function(name) {
-            this.name = name;
-        },
-        say_hello: function() {
-            console.log("hello", this.name);
-        },
-    });
-
-    var MySpanishClass = MyClass.extend({
-        say_hello: function() {
-            this._super();
-            console.log("translation in Spanish: hola", this.name);
+    local.ProductsWidget = instance.Widget.extend({
+        template: "ProductsWidget",
+        init: function(parent, products, color) {
+            this._super(parent);
+            this.products = products;
+            this.color = color;
         },
     });
-
-    var my_object = new MySpanishClass("Bob");
-    my_object.say_hello();
 
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
 }
