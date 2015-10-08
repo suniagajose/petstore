14b135b178b45bfbce057f6c1d1bd926b62069f0
Jose Suniaga <josemiguel@vauxoo.com>
2015-10-08 10:46:54 -0430

    [REF] petstore.js, petstore.xml, petstore.css: Exercise for Widget properties and events

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -3,19 +3,37 @@ openerp.oepetstore = function(instance, local) {
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;
 
-    local.HomePage = instance.Widget.extend({
-        start: function() {
-            var myWidget = new local.MyWidget(this);
-            this.widget = myWidget;
-            this.widget.on("change:name", this, this.name_changed);
-            this.widget.set("name", "Nicolas");
+
+    local.ColorInputWidget = instance.Widget.extend({
+        template: "ColorInputWidget",
+        events: {
+            'change input': 'input_changed'
         },
-        name_changed: function() {
-            console.log("The new value of the property 'name' is", this.widget.get("name"));
-        }
+        start: function() {
+            this.input_changed();
+            return this._super();
+        },
+        input_changed: function() {
+            var color = [
+                "#",
+                this.$(".oe_color_red").val(),
+                this.$(".oe_color_green").val(),
+                this.$(".oe_color_blue").val(),
+            ].join('');
+            this.set("color", color);
+        },
     });
 
-    local.MyWidget = instance.Widget.extend({
+    local.HomePage = instance.Widget.extend({
+        template: "HomePage",
+        start: function() {
+            this.colorInput = new local.ColorInputWidget(this);
+            this.colorInput.on("change:color", this, this.color_changed);
+            return this.colorInput.appendTo(this.$el);
+        },
+        color_changed: function() {
+            this.$(".oe_color_div").css("background-color", this.colorInput.get("color"));
+        }
     });
 
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
